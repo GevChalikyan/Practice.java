90f83fffc2f73a068b2df39898774f96ca165f43
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-12-15 22:09:20 -0800

    Refactored for Accepting Arguments Main will accept an argument stating which algorithm to run with logic for each case and error checking

--- src/main.java
+++ src/main.java
@@ -1,7 +1,32 @@
-import DivideAndConquer.MergeSort;
+import Algorithms.DivideAndConquer;
 
 class main {
 	public static void main(String[] args) {
-		MergeSort.main(null);
+
+		if(args.length == 0) {
+			System.err.println("Argument Expected");
+			System.exit(1);
+		} else {
+
+			String inputFile = "../data/input.txt";
+
+
+
+			if(args.length == 2) {
+				inputFile = args[1];
+			}
+			
+			switch(args[0]) {
+				case "MergeSort":
+					DivideAndConquer.main(new String[] { inputFile, args[0] });
+					break;
+
+				default:
+					System.err.println(args[0] + " Not Recognized");
+					System.exit(1);
+			}
+			
+		}
+
 	}
 }
