305802ee44e829f0fdb97a407bcf0e12ef3781b3
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-12-17 22:32:36 -0800

    Began Logic Recursive implementation of merge sort No sorting logic yet

--- src/Algorithms/DivideAndConquer.java
+++ src/Algorithms/DivideAndConquer.java
@@ -3,6 +3,7 @@ package Algorithms;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class DivideAndConquer {
 
@@ -12,7 +13,7 @@ public class DivideAndConquer {
 
 	private static int[] input;
 
-	
+
 
 	// ************* //
 	// MAIN FUNCTION //
@@ -20,20 +21,43 @@ public class DivideAndConquer {
 
 	public static void main(String[] args) {
 
-		String inputFile = "input.txt";
-
-
-
-		if(args.length != 0) {
-			inputFile = args[0];
-		}
-
-		try(Scanner scanner = new Scanner(new File(inputFile))){
-			readInput(scanner);
-		} catch(Exception e) {
-			e.printStackTrace();
+		if(args.length == 0) {
+			System.err.println("Argument Expected");
 			System.exit(1);
+		} else {
+			
+			String inputFile = "input.txt";
+			
+			
+			
+			if(args.length == 2) {
+				inputFile = args[1];
+			}
+
+			try(Scanner scanner = new Scanner(new File(inputFile))){
+				readInput(scanner);
+			} catch(Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			switch(args[0]) {
+				case "MergeSort":
+					System.out.println(
+						"Input:  " + Arrays.toString(input) + '\n' +
+						"Result: " + Arrays.toString(MergeSort(input))
+						);
+					break;
+
+				default:
+					System.err.println(args[0] + " Not Recognized");
+					System.exit(1);
+			}
+			
 		}
+
+		
+
 	}
 
 
@@ -57,10 +81,35 @@ public class DivideAndConquer {
 
 	public static int[] MergeSort(int[] arr) {
 
-		int[] result = new int[input.length];
+    int[] result = MergeSortHelper(arr);
 
-		return null;
+		return result;
 
 	}
+  
+  private static int[] MergeSortHelper(int[] arr) {
+
+    System.out.println("IN:\n\t" + Arrays.toString(arr));
+
+    if(arr.length > 2) {
+
+      final int BEGIN = 0;
+      final int END = arr.length;
+      final int MIDDLE = (BEGIN + END) / 2;
+
+      int[] left = MergeSortHelper(Arrays.copyOfRange(arr, BEGIN, MIDDLE));
+      int[] right = MergeSortHelper(Arrays.copyOfRange(arr, MIDDLE, END));
+
+      arr = IntStream.concat(Arrays.stream(left), Arrays.stream(right)).toArray();
+
+    }
+      
+    //
+    //  Sorting Logic
+    //
+    
+    return arr;
+
+  }
 
 }
